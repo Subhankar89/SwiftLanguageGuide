ef7cd9c21b4d7fd75fa91f0d5b4f12aede443ddf
Subhankar Acharya <subhankar.acharya@verizon.com>
2021-10-04 15:07:01 +0530

    added custom operator

--- SwiftDashboard/Advanced_Operators.swift
+++ SwiftDashboard/Advanced_Operators.swift
@@ -164,3 +164,93 @@ if twoThree == anotherTwoThree {
 // Prints "These two vectors are equivalent."
 
 //MARK:- Custom Operators
+
+/// You can declare and implement your own custom operators in addition to the standard operators provided by Swift.
+
+/// New operators are declared at a global level using the operator keyword, and are marked with the prefix, infix or postfix modifiers
+
+prefix operator +++
+
+
+extension Vector2D {
+    static prefix func +++ (vector: inout Vector2D) -> Vector2D {
+        vector += vector
+        return vector
+    }
+}
+
+var toBeDoubled = Vector2D(x: 1.0, y: 4.0)
+let afterDoubling = +++toBeDoubled
+// toBeDoubled now has values of (2.0, 8.0)
+// afterDoubling also has values of (2.0, 8.0)
+
+/// Precedence for Custom Infix Operators
+
+infix operator +-: AdditionPrecedence
+extension Vector2D {
+    static func +- (left: Vector2D, right: Vector2D) -> Vector2D {
+        return Vector2D(x: left.x + right.x, y: left.y - right.y)
+    }
+}
+let firstVector = Vector2D(x: 1.0, y: 2.0)
+let secondVector = Vector2D(x: 3.0, y: 4.0)
+let plusMinusVector = firstVector +- secondVector
+// plusMinusVector is a Vector2D instance with values of (4.0, -2.0)
+
+///You don’t specify a precedence when defining a prefix or postfix operator. However, if you apply both a prefix and a postfix operator to the same operand, the postfix operator is applied first.
+
+//MARK:- Result Builders
+
+///A result builder is a type you define that adds syntax for creating nested data, like a list or tree, in a natural, declarative way.
+
+protocol Drawable {
+    func draw() -> String
+}
+
+struct Line: Drawable {
+    var elements: [Drawable]
+    func draw() -> String {
+        return elements.map { $0.draw() }.joined(separator: "")
+    }
+}
+
+struct Text: Drawable {
+    var content: String
+    init(_ content: String) { self.content = content }
+    func draw() -> String { return content }
+}
+struct Space: Drawable {
+    func draw() -> String { return " " }
+}
+struct Stars: Drawable {
+    var length: Int
+    func draw() -> String { return String(repeating: "*", count: length) }
+}
+struct AllCaps: Drawable {
+    var content: Drawable
+    func draw() -> String { return content.draw().uppercased() }
+}
+
+let name: String? = "Ravi Patel"
+let manualDrawing = Line(elements: [
+    Stars(length: 3),
+    Text("Hello"),
+    Space(),
+    AllCaps(content: Text((name ?? "World") + "!")),
+    Stars(length: 2),
+    ])
+print(manualDrawing.draw())
+// Prints "***Hello RAVI PATEL!**"
+
+@resultBuilder
+struct DrawingBuilder {
+    static func buildBlock(_ components: Drawable...) -> Drawable {
+        return Line(elements: components)
+    }
+    static func buildEither(first: Drawable) -> Drawable {
+        return first
+    }
+    static func buildEither(second: Drawable) -> Drawable {
+        return second
+    }
+}

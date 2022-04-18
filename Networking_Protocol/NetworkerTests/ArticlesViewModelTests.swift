af039f2108230a8f921279600a049d5014b82789
Subhankar Acharya <subhankar.acharya@verizon.com>
2022-04-18 14:15:49 +0530

    added test

--- Networking_Protocol/NetworkerTests/ArticlesViewModelTests.swift
+++ Networking_Protocol/NetworkerTests/ArticlesViewModelTests.swift
@@ -7,6 +7,25 @@ import XCTest
 import Combine
 @testable import Networker
 
+///mock networker will implement the Networking protocol but return hard-coded values for each request
+class MockNetworker: Networking {
+  func fetch(_ request: Request) -> AnyPublisher<Data, URLError> {
+    let outputData: Data
+    switch request {
+    case is ArticleRequest:
+      let article = Article(name: "Article Name", description: "Article Description", image: URL(string: "https://image.com")!, id: "Article ID", downloadedImage: nil)
+      let articleData = ArticleData(article: article)
+      let articles = Articles(data: [articleData])
+      outputData = try! JSONEncoder().encode(articles)
+    default:
+      outputData = Data()
+    }
+    return Just<Data>(outputData)
+      .setFailureType(to: URLError.self)
+      .eraseToAnyPublisher()
+  }
+}
+
 class ArticlesViewModelTests: XCTestCase {
   // swiftlint:disable:next implicitly_unwrapped_optional
   var viewModel: ArticlesViewModel!
@@ -14,8 +33,26 @@ class ArticlesViewModelTests: XCTestCase {
 
   override func setUpWithError() throws {
     try super.setUpWithError()
+    ///create a new view model instance and inject your stub networker
+    viewModel = ArticlesViewModel(networker: MockNetworker())
   }
-
+  
+  ///verify that your view model fetches articles and decodes them correctly
   func testArticlesAreFetchedCorrectly() {
+    XCTAssert(viewModel.articles.isEmpty)
+    ///Expectations are used to test asynchronous code.
+    ///The expectation will keep the test going until the expectation is fulfilled or a timer runs out.
+    let expectation = XCTestExpectation(description: "Article received")
+    ///you subscribe to changes to the articles array and wait until it’s not empty
+    viewModel.$articles.sink { articles in
+      guard !articles.isEmpty else { return }
+      XCTAssertEqual(articles[0].id, "Article ID")
+      expectation.fulfill()
+    }
+    .store(in: &cancellables)
+    ///start the fetching and decoding process
+    viewModel.fetchArticles()
+    ///tell XCTest to wait for 0.1 seconds until your expectation is fulfilled
+    wait(for: [expectation], timeout: 0.1)
   }
 }
